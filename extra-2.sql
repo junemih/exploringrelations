aec4cdc992c2012fefd594950c4e080110f91b63
junemih <junehong@uw.edu>
2023-04-06 22:54:31 -0700

    final changes

---
+++ extra-2.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(DISTINCT gender) 
+FROM persons;
+
+-- 8
---
+++ extra-3.sql
@@ -0,0 +1,4 @@
+SELECT MAX(age) AS max_age, MIN(age) AS min_age, AVG(age) AS avg_age
+FROM persons;
+
+-- The minimum age is 18, the maximum age is 110, and the average individual is in their early 60s
